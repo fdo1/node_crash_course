bf395537f168d51e600e29a0d0291cc694326fca
Fernando <fernando.madser@gmail.com>
2019-04-19 09:16:16 -0500

    Includes Handlebars as templating engine

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const express = require('express');
 const path = require('path');
 const members = require('./members');
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 // Initt middleware
 //app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage router
+app.get('/', (req, res) => { res.render('index', {
+    title: 'Members App - Crashcourse',
+    members
+  });
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
